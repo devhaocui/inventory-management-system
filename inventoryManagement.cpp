b64daaac7ac016618ac34085e7d4269086188d88
devhaocui <devhaocui@gmail.com>
2024-06-22 01:45:35 -0700

    added display functions
    display() + displayCategory()

--- inventoryManagement.cpp
+++ inventoryManagement.cpp
@@ -114,9 +114,12 @@ void invManage::userOptionMenu() {
   int itemQuantity;
   std::cout << "Option 1: \n";
   std::cout << "Option 2: \n";
-  std::cout << "Option 3: Add An Item \n";
-  std::cout << "Option 4: Delete An Item\n";
-  std::cout << "Option 5: Withdraw An Item\n";
+  std::cout << "Option 3: Add an item \n";
+  std::cout << "Option 4: Delete an item\n";
+  std::cout << "Option 5: Withdraw an item\n";
+  std::cout << "Option 6: Search for an item\n";
+  std::cout << "Option 7: Display all items\n";
+  std::cout << "Option 8: Display by category\n";
 
   std::cout << "Choose your option\n";
   std::cin >> userInput;
@@ -153,5 +156,76 @@ void invManage::userOptionMenu() {
       withdrawItem(itemName, itemQuantity);
       break;
     }
+
+    case 6: {
+      std::cout << "Input (1)ItemName to start searching\n";
+      std::cin >> itemName;
+      searchItem(itemName);
+    }
+
+    case 7: {
+      display();
+    }
+
+    case 8: {
+      std::cout << "Input (1)Category Name to start displaying\n";
+      std::cin >> itemCategory;
+      displayCategory(itemCategory);
+    }
+  }
+}
+
+void invManage::searchItem(std::string iName) {
+  bool itemFound = false;
+  int width = 10;
+  std::vector<std::string> vec1;
+  std::fstream myFile;
+  myFile.open("item.csv", std::ios::in);
+  std::string line;
+  while (getline(myFile, line, ',')){
+    vec1.push_back(line);
+  }
+  myFile.close();
+
+  for (size_t i{0}; i < vec1.size(); i += 3) {
+    if (iName == vec1[i]) {
+      itemFound = true;
+      std::cout << "\n\n====Item Found====\nItemName |" << std::setw(width) << " Category |" << std::setw(width) << " Quantity\n"
+                << iName << std::setw(width+2) << vec1[i + 1] << std::setw(width+1) << vec1[i + 2] << "\n";
+    }
+  }
+  if (!itemFound)
+    std::cout << "Did not find (" << iName << ") in the store.\n";
+}
+
+void invManage::display() {
+  std::vector<std::string> vec1;
+  std::fstream myFile;
+  myFile.open("item.csv", std::ios::in);
+  std::string line;
+  while (getline(myFile, line, ',')){
+    vec1.push_back(line);
+  }
+  myFile.close();
+
+  std::cout << "Name |" << std::setw(10) << " Category |" << std::setw(10) << " Quantity\n";
+  for (size_t i{0}; i < vec1.size(); i += 3) {
+    std::cout << vec1[i] << " | " << vec1[i+1] << " | " << vec1[i+2] << "\n";
+  }
+}
+
+void invManage::displayCategory(std::string iCategory) {
+  std::vector<std::string> vec1;
+  std::fstream myFile;
+  myFile.open("item.csv", std::ios::in);
+  std::string line;
+  while (getline(myFile, line, ',')){
+    vec1.push_back(line);
+  }
+  myFile.close();
+  std::cout << "Name |" << std::setw(10) << " Category |" << std::setw(10) << " Quantity\n";
+  for (size_t i{0}; i < vec1.size(); i += 3) {
+    if (iCategory == vec1[i+1])
+      std::cout << vec1[i] << " | " << vec1[i+1] << " | " << vec1[i+2] << "\n";
   }
 }
--- inventoryManagement.h
+++ inventoryManagement.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <iomanip>
 
 class invManage{
 private:
@@ -15,15 +16,16 @@ public:
   //void userLoginMenu();
   //bool isNumber(std::string item);
   void userOptionMenu();
-  void addItem (std::string iName, std::string iCategory, int iQuantity);
+  void addItem (std::string, std::string, int);
   void deleteItem (std::string);
   void withdrawItem (std::string, int);
-  bool categoryExists (std::string iCategory);
-  bool itemExists(std::string iName);
-  void displayItem ();
-  void displayByName();
-  void displayByCategory();
-  void displayByQuantity();
+  bool categoryExists (std::string);
+  bool itemExists(std::string);
+  void searchItem (std::string);
+  void display();
+  void displayByName (std::string);
+  void displayCategory (std::string);
+  void displayByQuantity ();
 };
 
 
